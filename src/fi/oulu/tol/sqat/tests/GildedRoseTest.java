e09aa5ab307054b6ce968b287f1964b6ea540663
dachen <dachen@STUPID-PC712.student.yo.oulu.fi>
2017-09-21 14:58:05 +0300

    Dalun Chen added five test cases and one case failed

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fi.oulu.tol.sqat.GildedRose;
@@ -19,24 +20,68 @@ public class GildedRoseTest {
 //   Item("Sulfuras, Hand of Ragnaros", 0, 80));
 //   Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 //   Item("Conjured Mana Cake", 3, 6));
+	
+	GildedRose store;
+	List<Item> items;
+	@Before
+	public void setUp(){
+		store = new GildedRose();
+	}
 
 	@Test
 	public void testUpdateEndOfDay_AgedBrie_Quality_10_11() {
 		// Arrange
-		GildedRose store = new GildedRose();
+		//GildedRose store = new GildedRose();
 		store.addItem(new Item("Aged Brie", 2, 10) );
 		
 		// Act
 		store.updateEndOfDay();
 		
 		// Assert
-		List<Item> items = store.getItems();
+		items = store.getItems();
+		//List<Item> items = store.getItems();
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
     
+	// My codes
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_common_item_SellIn_decrease_1_per_day() {
+		//fail("Test not implemented");
+		store.addItem(new Item("common item", 3, 3));
+		store.updateEndOfDay();
+		items = store.getItems();
+		Item commonItem = items.get(0);
+		assertEquals("2", 2, commonItem.getSellIn());
+	}
+	@Test
+	public void testUpdateEndOfDay_common_item_Quality_decrease_1_per_day() {
+		//fail("Test not implemented");
+		store.addItem(new Item("common item", 2, 2));
+		store.updateEndOfDay();
+		items = store.getItems();
+		Item commonItem = items.get(0);
+		assertEquals("1", 1, commonItem.getQuality());
+	}
+	@Test
+	public void testUpdateEndOfDay_Quality_is_never_negative(){
+		store.addItem(new Item("common item", 2, 0));
+		items = store.getItems();
+		Item commonItem = items.get(0);
+		assertEquals("quality == 0", 0, commonItem.getQuality());
+	}
+	@Test
+	public void testUpdateEndOfDay_commonItem_quality_degrades_twice_as_fast_when_sellIn_equals_0(){
+		store.addItem(new Item("common item", -1, 4));
+		items = store.getItems();
+		Item commonItem = items.get(0);
+		assertEquals("quality == 2", 2, commonItem.getQuality());
+	}
+	@Test
+	public void testUpdateEndOfDay_AgedBrie_Quality_50(){
+		store.addItem(new Item("Aged Brie", 10, 50));
+		items = store.getItems();
+		Item agedBrie = items.get(0);
+		assertEquals("quality remains 50", 50, agedBrie.getQuality());
 	}
 }
